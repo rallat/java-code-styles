5ced84c426225cdf823d3253528dd499a39f3570
Israel Camacho <icamacho@twitter.com>
2015-02-23 21:09:13 -0800

    Replace Square's code style for Fabric's code style

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Installs Square's IntelliJ configs into your user configs.
+# Installs Fabric's IntelliJ configs into your user configs.
 
-echo "Installing Square code style configs..."
+echo "Installing Fabric code style configs..."
 
 for i in $HOME/Library/Preferences/IntelliJIdea*/codestyles \
          $HOME/Library/Preferences/IdeaIC*/codestyles \
@@ -15,4 +15,4 @@ done
 
 echo "Done."
 echo ""
-echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'."
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Fabric'."
